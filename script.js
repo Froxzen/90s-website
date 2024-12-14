b3564a4f3e71cf3c5463b58e13a9437f5fe00839
Froxzen <harvardchong1@gmail.com>
2024-12-14 22:30:04 +1100

    Added styles and community page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const postButton = document.querySelector(".postBox_button");
+
+	postButton.addEventListener("click", function () {
+		alert("This feature doesn't exist :(");
+	});
+});
